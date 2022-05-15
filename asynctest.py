f504df1ad9939b53c41c3497a2cda218f384522a
Andrew Scheer <apscheer@protonmail.com>
2022-05-15 08:40:58 -0600

    added code to asynctest.py

---
+++ asynctest.py
@@ -0,0 +1,16 @@
+import asyncio
+
+async def count():
+    for i in [1,2,3,4,5]:
+        print(i)
+        await asyncio.sleep(1)
+async def count2():
+    for letter in "Andrew":
+        print(letter)
+        await asyncio.sleep(2)
+async def main():
+    await asyncio.gather(count(),count2())
+
+
+asyncio.run(main())
+print("Done")
