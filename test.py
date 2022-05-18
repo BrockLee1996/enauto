358c3d8b0cf9423368baff2841a608a56aacf1cf
Andrew Scheer <apscheer@protonmail.com>
2022-05-17 18:32:07 -0600

    imported os,datetime. Added Logic class

---
+++ test.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+class UI():
+    def __init__(self):
+        self.greeting = str((datetime.datetime.now()).strftime("%Y-%m-%d %H:%M"))
+        print(self.greeting)
+class Logic():
+    def time_of_day():
+        print("It is x time of day")
+
+UI()                        
