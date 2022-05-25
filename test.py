75707bd0a3defcebd2353a6de16debd20d891435
Andrew Scheer <apscheer@protonmail.com>
2022-05-25 08:37:06 -0600

    Added to the Parser function

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import datetime
 class UI(): 
     def __init__(self):
         self.greeting = str((datetime.datetime.now()).strftime("%Y-%m-%d %H:%M"))
-        Parser("entr","Commands.Entry") # Parser[0] is what the user sees, Parser[1] is where code is called from
+        Parser("entr()# ","Commands.Entry.") # Parser[0] is what the user sees, Parser[1] is where code is called from
 #class Logic():
 #    def time_of_day():
 #        print("It is x time of day")
@@ -11,10 +11,13 @@ class UI():
 class Commands:
     class Entry:
         def greeter(arg):
-            print("Hi" + arg)
+            print("Hi " + arg)
 
 def Parser(prompt,cmdclass):
     while True:
         usercmd = input(prompt)
+        usercmd = usercmd.split(" ")
+        command = cmdclass + usercmd[0] + "("+"'"+usercmd[1]+"'"+")" # The arguments need to passed as a string, otherwise eval thinks this is some type of variable
+        eval(command)
 
 UI()                        
