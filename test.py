3859a6bd6a462cbfee53ff035e94ceb8414a990b
Andrew Scheer <apscheer@protonmail.com>
2022-05-25 07:18:29 -0600

    Added class Commands.Entry, Added Parser with args

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 import os
 import datetime
-class UI():
+class UI(): 
     def __init__(self):
         self.greeting = str((datetime.datetime.now()).strftime("%Y-%m-%d %H:%M"))
-        #print(self.greeting)
+        Parser("entr","Commands.Entry") # Parser[0] is what the user sees, Parser[1] is where code is called from
 #class Logic():
 #    def time_of_day():
 #        print("It is x time of day")
+
+class Commands:
+    class Entry:
+        def greeter(arg):
+            print("Hi" + arg)
+
+def Parser(prompt,cmdclass):
+    while True:
+        usercmd = input(prompt)
 
 UI()                        
